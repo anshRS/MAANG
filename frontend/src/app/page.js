d89237f594eb1c294fb15a87265989f9dada1dbc
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-16 00:46:24 +0530

    Updated home layout

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,6 +2,8 @@
 import Navbar from "@/components/Navbar.js";
 import Card from "@/components/Card.js";
 import BarGraph from "@/components/BarGraph";
+import LineChart from "@/components/LineChart";
+import PieChart from "@/components/PieChart";
 import { useEffect, useState } from "react";
 import axiosInst from "@/utils/axios";
 
@@ -25,10 +27,11 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-screen bg-[#212121]">
+      <div className="h-screen bg-[#212121] flex items-center">
         <Navbar />
+        <div className="w-full p-4 flex flex-col gap-3"> 
 
-        <div className="grid grid-cols-5 w-full items-center justify-center gap-4 p-2">
+        <div className="grid grid-cols-5 w-full items-center justify-center gap-4">
           
             <Card title="Number of Records" value={numberOfRecords} />
             <Card title="Mean Salary" value={numberOfRecords} />
@@ -38,27 +41,28 @@ export default function Home() {
         
         </div>
 
-        <div className="grid grid-cols-3 gap-2.5 justify-center">
-          <div className="flex col-span-2 items-center justify-center rounded-lg bg-black p-4  h-96">
-            <BarGraph />
+        <div className="grid grid-cols-3 gap-3 justify-center">
+          <div className="flex col-span-2 items-center justify-center rounded-lg bg-black p-4 h-80">
+            <LineChart />
           </div>
             <div className="flex items-center justify-center bg-black p-4 text-white rounded-lg ">
               Sub Graph
             </div>
         </div>
 
-        <div className="flex w-full items-center justify-center gap-2.5 h-44 p-2">
-          <div className="flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full">
-            Main Graph
+        <div className="flex items-center justify-center gap-3 h-60 ">
+          <div className="flex basis-1/3 p-4 items-center justify-center bg-black text-white rounded-lg h-full">
+            <PieChart />
           </div>
-          <div className="flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full">
-            Main Graph
+          <div className="flex basis-1/3 p-4 items-center justify-center bg-black text-white rounded-lg h-full">
+            <BarGraph />
           </div>
           <div className="flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full">
             Main Graph
           </div>
         </div>
       </div>
+        </div>
     </>
   );
 }
