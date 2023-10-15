520fcf66b01a8ac1df4cf1775c896d531469f7bc
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-16 01:54:00 +0530

    Updated Navbar design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
+import {AiOutlineHome, AiOutlinePieChart} from "react-icons/ai"
+import {BiBarChartAlt2} from "react-icons/bi"
+
 
 
 const Test = () => {
   return (
-    <div className='h-screen bg-black flex flex-col items-center justify-center gap-16 w-[250px]'>
-      <Link href="" className='text-white'>
-        Home
+    <div className='h-screen bg-black flex flex-col  justify-center gap-14 w-[250px] p-4'>
+      <Link href="" className='text-white flex items-center gap-6 text-xl '>
+        <AiOutlineHome />
+        <h3>Home</h3>
       </Link>
-      <Link href="" className='text-white'>
-        Bar Chart
+      <Link href="" className='text-white flex items-center gap-6 text-xl'>
+        <BiBarChartAlt2 />
+        <h3>Bar Chart</h3>
       </Link>
-      <Link href="" className='text-white'>
-        Pie Chart
+      <Link href="" className='text-white flex items-center gap-6 text-xl'>
+        <AiOutlinePieChart />
+        <h3>Pie Chart</h3>
       </Link>
       
     </div>
