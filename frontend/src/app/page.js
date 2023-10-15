724bc305bf50a99f4cd003eaf557ee08ee43f4b4
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-15 18:54:00 +0530

    Created separate Card component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,21 +1,21 @@
-import Navbar from "../components/Navbar.js"
+import Navbar from "@/components/Navbar.js"
+import Card from "@/components/Card.js"
+import { CardsData } from "@/constants/constants"
+
 
 
 export default function Home() {
   return (
     <>
-    <div className= ''>
+    
       <Navbar />
-    </div>
 
-
-    <div className=''>
-      <div className='flex w-full items-center justify-center gap-2.5 h-20 p-2'>
-        <div className='flex basis-1/5 items-center justify-center bg-black text-white rounded-lg h-full'>number of entry</div>
-        <div className='flex basis-1/5 items-center justify-center bg-black text-white rounded-lg h-full'>mean</div>
-        <div className='flex basis-1/5 items-center justify-center bg-black text-white rounded-lg h-full'>median</div>
-        <div className='flex basis-1/5 items-center justify-center bg-black text-white rounded-lg h-full'>25 percentile</div>
-        <div className='flex basis-1/5 items-center justify-center bg-black text-white rounded-lg h-full'>75 percentile</div>
+      <div className='grid grid-cols-5 w-full items-center justify-center gap-4 p-2'>
+        {
+          CardsData.map((item) => (
+            <Card key={item.title} title={item.title} />
+          ))
+        }
       </div>
 
       <div className='flex w-full items-center justify-center gap-2.5 h-96 p-2'>
@@ -29,7 +29,7 @@ export default function Home() {
         <div className='flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full'>Main Graph</div>
       </div>
 
-    </div>
+    
     </>
   )
 }
---
+++ frontend/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({title}) => {
+  return (
+    <div className='flex flex-col items-center p-4 bg-black text-white rounded-lg h-full hover:scale-105'>
+        <h1 className=' text-lg'>
+        1,000,000
+        </h1>
+        <h1 className='text-sm'> {title}</h1>
+    </div>
+  )
+}
+
+export default Card
