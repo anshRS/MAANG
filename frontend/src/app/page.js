6949ab63d63ffa8d740ed19aa478899086ad2931
Aditya <codecommander03@gmail.com>
2023-10-15 23:05:32 +0530

    Updated Home Layout

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,7 @@
-import Navbar from "@/components/Navbar.js"
-import Card from "@/components/Card.js"
-import { CardsData } from "@/constants/constants"
-import BarGraph from "@/components/BarGraph"
-
-
+import Navbar from "@/components/Navbar.js";
+import Card from "@/components/Card.js";
+import { CardsData } from "@/constants/constants";
+import BarGraph from "@/components/BarGraph";
 
 export default function Home() {
 
@@ -18,19 +16,27 @@ export default function Home() {
           ))}
         </div>
 
-      <div className='flex w-full items-center justify-center gap-2.5 h-96 p-2'>
-        <BarGraph/>
-        <div className='flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full'>Sub Graph</div>
-      </div>
+        <div className="grid grid-cols-3 gap-2.5 justify-center">
+          <div className="flex col-span-2 items-center justify-center rounded-lg bg-black p-4  h-96">
+            <BarGraph />
+          </div>
+            <div className="flex items-center justify-center bg-black p-4 text-white rounded-lg ">
+              Sub Graph
+            </div>
+        </div>
 
-      <div className='flex w-full items-center justify-center gap-2.5 h-44 p-2'>
-        <div className='flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full'>Main Graph</div>
-        <div className='flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full'>Main Graph</div>
-        <div className='flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full'>Main Graph</div>
+        <div className="flex w-full items-center justify-center gap-2.5 h-44 p-2">
+          <div className="flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full">
+            Main Graph
+          </div>
+          <div className="flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full">
+            Main Graph
+          </div>
+          <div className="flex basis-1/3 items-center justify-center bg-black text-white rounded-lg h-full">
+            Main Graph
+          </div>
+        </div>
       </div>
-    </div>
-
-    
     </>
-  )
+  );
 }
