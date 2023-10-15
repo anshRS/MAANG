ea0bb240b800cf32b04f828fe0fbd4b0dfa66787
anshRS <anshraj0426@gmail.com>
2023-10-16 02:53:17 +0530

    Modified salary api route

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('totalrecords/', views.totalRecordsApi, name='total-records'),   
-    path('meansalary/', views.meanSalaryApi, name='mean-salary'), 
-    path('mediansalary/', views.medianSalaryApi, name='median-salary'),
-    path('percentilesalary/', views.percentileSalaryApi, name='percentile-salary'),
+    path('salaryrecords/', views.SalaryApi, name='total-records'),       
 ]
