7a534accdfaad9e207e68bde29efff7249ca9b9d
anshRS <anshraj0426@gmail.com>
2023-10-16 06:53:58 +0530

    Modified Navbar design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,15 +10,15 @@ const Navbar = () => {
   return (
     <div className='h-screen bg-[#0c1012] flex flex-col gap-8 w-[250px] p-6 pt-14'>
       <Image src={logo} alt='logo' className='rounded-full object-cover' />
-      <Link href="" className='text-white flex items-center rounded-2xl px-2 py-3 gap-4 text-2xl hover:bg-[#3FC1C9] hover:scale-110 '>
+      <Link href="" className='text-white flex items-center rounded-2xl px-2 py-3 gap-4 text-2xl hover:bg-[#3FC1C9] hover:scale-110 hover:text-black'>
         <AiOutlineHome />
         <h3>Home</h3>
       </Link>
-      <Link href="" className='text-white flex items-center rounded-2xl px-2 py-3 gap-4 text-2xl hover:bg-[#3FC1C9] hover:scale-110'>
+      <Link href="" className='text-white flex items-center rounded-2xl px-2 py-3 gap-4 text-2xl hover:bg-[#3FC1C9] hover:scale-110 hover:text-black'>
         <BiBarChartAlt2 />
         <h3>Bar Chart</h3>
       </Link>
-      <Link href="" className='text-white flex items-center rounded-2xl px-2 py-3 gap-4 text-2xl hover:bg-[#3FC1C9] hover:scale-110'>
+      <Link href="" className='text-white flex items-center rounded-2xl px-2 py-3 gap-4 text-2xl hover:bg-[#3FC1C9] hover:scale-110 hover:text-black'>
         <AiOutlinePieChart />
         <h3>Pie Chart</h3>
       </Link>
