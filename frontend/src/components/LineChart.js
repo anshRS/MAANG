280ad53cc00e29e42054bbdd8fad4cc3bc337a73
anshRS <anshraj0426@gmail.com>
2023-10-16 07:50:48 +0530

    Developed List component

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Chart as ChartJS,
     LineElement,
@@ -8,6 +8,7 @@ import {
     PointElement
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import axiosInst from '@/utils/axios';
 
 ChartJS.register(
     LineElement,
@@ -17,27 +18,57 @@ ChartJS.register(
 )
 
 function line() {
+    const [lineChart, setLineChart] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosInst.get('/api/v1/listdata');
+                console.log(response.data.top5cities)
+                setLineChart(response.data.top5cities);    
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, [])
+
+    const labels = lineChart.map((item, i)=> {
+        if(i == 0) {
+            return
+        }
+        return(
+            
+            item[0]
+        )
+    })
+
+    const dataInfo = lineChart.map((item) => {
+        return (
+            item[1]
+        )
+    })
+
     const data = {
-        labels: ['New York', 'Las Vegas', 'Texas'],
+        labels: labels,
         datasets: [
             {
                 label: 'With H1B',
-                data: [5, 2, 9],
+                data: dataInfo,
                 backgroundColor: '#1FAB89',
                 borderColor: '#1FAB89',
                 pointBorderColor: 'white',
                 fill: true,
                 tension: 0.4
             },
-            {
-                label: 'Without H1B',
-                data: [1, 7, 3],
-                backgroundColor: 'aqua',
-                borderColor: '#3FC1C9',
-                pointBorderColor: '#3FC1C9',
-                fill: true,
-                tension: 0.4
-            }
+            // {
+            //     label: 'Without H1B',
+            //     data: [1, 7, 3],
+            //     backgroundColor: 'aqua',
+            //     borderColor: '#3FC1C9',
+            //     pointBorderColor: '#3FC1C9',
+            //     fill: true,
+            //     tension: 0.4
+            // }
         ]
     }
 
---
+++ frontend/src/components/ListComp.jsx
@@ -0,0 +1,50 @@
+import axiosInst from '@/utils/axios';
+import React, { useEffect, useState } from 'react'
+
+const ListComp = () => {
+    const [lineChart, setLineChart] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosInst.get('/api/v1/listdata');
+                // console.log(response.data.top5cities)
+                // setLineChart(response.data.top5cities);    
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, [])
+    return (
+        <div className='w-full'>
+
+            {
+                lineChart.map((item, i)=>{
+                    if(i == 0){
+                        return
+                    }
+                    return(<>
+                        <div className='text-white flex justify-between text-start px-5 pt-2'>
+                            <h1>{item[0]}</h1>
+                            <p>{item[1]}</p>            
+                            
+                        </div>
+                        </>
+                    )
+
+                })
+            }
+
+            {/* <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">Profile</li>
+                <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Settings</li>
+                <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Messages</li>
+                <li class="w-full px-4 py-2 rounded-b-lg">Download</li>
+            </ul> */}
+
+
+        </div>
+    )
+}
+
+export default ListComp
